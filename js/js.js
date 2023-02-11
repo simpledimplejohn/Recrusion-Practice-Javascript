8b0d00a459127fbbe079a9b6a442b548e211f917
Simple Dimple John <simpledimplejohn@gmail.com>
2023-02-11 15:42:21 -0500

    double array functionally

--- js/js.js
+++ js/js.js
@@ -1,1 +1,11 @@
-console.log("you can run this");
+// console.log("you can run this");
+
+const originalArray = [1,2,3];
+const doubledArray = [];
+originalArray.forEach(function(element) {
+    const doubledElement = element * 2;
+    doubledArray.push(doubledElement);
+})
+console.log(originalArray);
+console.log(doubledArray);
+
