9fed84c0fccfe0d16fc13b54c52a85dbe3efc9cf
Simple Dimple John <simpledimplejohn@gmail.com>
2023-02-11 15:56:20 -0500

    add guessMyNumber

--- js/js.js
+++ js/js.js
@@ -6,6 +6,52 @@ originalArray.forEach(function(element) {
     const doubledElement = element * 2;
     doubledArray.push(doubledElement);
 })
-console.log(originalArray);
-console.log(doubledArray);
+// console.log(originalArray);
+// console.log(doubledArray);
 
+// map is a funcional method
+const newArray = originalArray.map(function(element) {
+    return element * 2;
+})
+// console.log(originalArray);
+// console.log(newArray);
+
+function addOne(num) {
+    return num + 1;
+}
+
+const num = 1;
+const newNum = addOne(num);
+// console.log(num, " ", newNum);
+
+function guessMyNumber(num) {
+    const myNumber = Math.floor(Math.random() * Math.floor(10)) + 1;
+    if(num === myNumber) {
+        return "you were right the number was ", myNumber;
+    } else {
+        return "Wrong ", num, " was not ", myNumber;
+    }
+}
+
+// console.log(guessMyNumber(5));
+
+function isFive(num) {
+    if(num === 5) {
+        return "five"
+    } else {
+        return "not five"
+    }
+}
+
+console.log(isFive(5));
+
+
+function guessMyNumber(num) {
+    const myNumber = Math.floor(Math.random() * Math.floor(10) ) + 1;
+    if (num === myNumber) {
+      return "You guessed right!" 
+    } else {
+      return "That wasn't it."
+    }
+}
+// console.log(guessMyNumber(5));
