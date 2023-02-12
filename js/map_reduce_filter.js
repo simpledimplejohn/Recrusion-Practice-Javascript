dd0261006856d979008a91f83d238dab9fe85711
Simple Dimple John <simpledimplejohn@gmail.com>
2023-02-12 09:53:50 -0500

    add reduce

--- js/map_reduce_filter.js
+++ js/map_reduce_filter.js
@@ -1,3 +1,14 @@
+/*
+Practice the following commands:
+array.prototype.map
+array.prototype.reduce
+array.prototype.filter
+
+similarities include:
+- do not change the array but transfer changes to a new array
+*/
+
+
 // map is non destructive 
 
 const numArray = [1,2,3,4];
@@ -11,5 +22,38 @@ let doubleArray = [];
 
 doubleArray = numArray.map(e => e * 3);
 
-console.log(doubleArray);
+// console.log(doubleArray);
+
+const newNumArray = [1,2,3,4]
+
+const summedArray = newNumArray.reduce(function(value, element) {
+    return element + value;
+})
+
+// console.log(summedArray)
+
+const friends = [
+    {
+      name: "Jasmine",
+      wantToDo: ["hike", "go out to eat", "swim"]
+    },
+    {
+      name: "Ada",
+      wantToDo: ["play games", "hike", "cook meals"]
+    },
+    {
+      name: "Desmond",
+      wantToDo: ["sleep", "swim", "play games"]
+    },
+    {
+      name: "Andres",
+      wantToDo: ["hike", "swim", "play games"]
+    }
+];
+
+const toDos = friends.reduce(function(array, friend) {
+    return array.concat(friend.wantToDo);
+}, []);
+
+console.log(toDos);
 
